83281b65598f956af797823f50065db4ff99e3e8
mercury131 <34038246+mercury131@users.noreply.github.com>
2024-05-16 09:37:27 +0300

    Update models.ts

--- pages/api/models.ts
+++ pages/api/models.ts
@@ -1,8 +1,5 @@
-import { OPENAI_API_HOST, OPENAI_API_TYPE, OPENAI_API_VERSION, OPENAI_ORGANIZATION } from '@/utils/app/const';
-
-import { OpenAIModel, OpenAIModelID, OpenAIModels } from '@/types/openai';
 import { LLAMA_API_HOST } from '@/utils/app/const';
-
+import { OpenAIModel, OpenAIModelID, OpenAIModels } from '@/types/openai';
 export const config = {
   runtime: 'edge',
 };
@@ -13,23 +10,9 @@ const handler = async (req: Request): Promise<Response> => {
       key: string;
     };
 
-    let url = `${OPENAI_API_HOST}/v1/models`;
-    if (OPENAI_API_TYPE === 'azure') {
-      url = `${OPENAI_API_HOST}/openai/deployments?api-version=${OPENAI_API_VERSION}`;
-    }
-
-    const response = await fetch(url, {
+    const response = await fetch(${LLAMA_API_HOST}/v1/models, {
       headers: {
         'Content-Type': 'application/json',
-        ...(OPENAI_API_TYPE === 'openai' && {
-          Authorization: `Bearer ${key ? key : process.env.OPENAI_API_KEY}`
-        }),
-        ...(OPENAI_API_TYPE === 'azure' && {
-          'api-key': `${key ? key : process.env.OPENAI_API_KEY}`
-        }),
-        ...((OPENAI_API_TYPE === 'openai' && OPENAI_ORGANIZATION) && {
-          'OpenAI-Organization': OPENAI_ORGANIZATION,
-        }),
       },
     });
 
@@ -40,48 +23,16 @@ const handler = async (req: Request): Promise<Response> => {
       });
     } else if (response.status !== 200) {
       console.error(
-        `OpenAI API returned an error ${response.status
-        }: ${await response.text()}`,
+        LLAMA API returned an error ${response.status}: ${await response.text()},
       );
-      throw new Error('OpenAI API returned an error');
+      throw new Error('LLAMA API returned an error');
     }
 
     let json = await response.json();
-    const llama_response = await fetch(`${LLAMA_API_HOST}/v1/models`, {
-      headers: {
-        'Content-Type': 'application/json'
-      },
-    });
-
-    if (llama_response.status === 401) {
-      return new Response(llama_response.body, {
-        status: 500,
-        headers: llama_response.headers,
-      });
-    } else if (llama_response.status !== 200) {
-      console.error(
-        `OpenAI API returned an error ${llama_response.status
-        }: ${await llama_response.text()}`,
-      );
-      throw new Error('OpenAI API returned an error');
-    }
-
-    const llama_json = await llama_response.json();
-    json.data.push(...llama_json.data)
-
-    const models: OpenAIModel[] = json.data
-      .map((model: any) => {
-        const model_name = (OPENAI_API_TYPE === 'azure') ? model.model : model.id;
-        for (const [key, value] of Object.entries(OpenAIModelID)) {
-          if (value === model_name) {
-            return {
-              id: model.id,
-              name: OpenAIModels[value].name,
-            };
-          }
-        }
-      })
-      .filter(Boolean);
+    const models = json.data.map((model: any) => ({
+      id: model.id,
+      name: model.name,
+    }));
 
     return new Response(JSON.stringify(models), { status: 200 });
   } catch (error) {
